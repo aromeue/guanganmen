a809426042802aae2dcc20aaadcc1c9dc70996ec
aromeue <alvaro@hitech-informatica.es>
2015-01-02 11:34:53 +0100

    v2.02-1134

--- js/PushInit.js
+++ js/PushInit.js
@@ -87,7 +87,7 @@ function registerPushwooshAndroid() {
 			  });
 
 				//projectid: "GOOGLE_PROJECT_ID", appid : "PUSHWOOSH_APP_ID"
-				pushNotification.onDeviceReady(/*{ projectid: "675581025503", appid: "E9024-6EF1C" }*/);
+				pushNotification.onDeviceReady({ projectid: "675581025503"/*, appid: "E9024-6EF1C"*/ });
 				
 				pushNotification.registerDevice(
 					function(token) {
