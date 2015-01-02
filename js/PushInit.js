07a33fd02b9909aded46a677aa81cba14c4bd6c2
aromeue <alvaro@hitech-informatica.es>
2015-01-02 12:49:27 +0100

    v2.02-1249

--- js/PushInit.js
+++ js/PushInit.js
@@ -56,9 +56,6 @@ function registerPush() {
 			$("#app-status-ul").append("<li>Error:"+error+"</li>");
 		},
 		{
-			"badge":"true",
-		    "sound":"true",
-		    "alert":"true",
 			"senderID":"675581025503",
 			"ecb":"onNotification"
 		});
@@ -192,12 +189,8 @@ var app = {
     //
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicity call 'app.receivedEvent(...);'
-    onDeviceReady: function() {	
-        /*initPushwoosh();*/
-		
-		var pushNotification = window.plugins.pushNotification;
-				
-		initPush();
+    onDeviceReady: function() {				
+		//initPush();
         app.receivedEvent('deviceready');
     },
 	onGoOffline: function() {
@@ -215,100 +208,11 @@ var app = {
 
         console.log('Received Event: ' + id);
 		alert(id);
-		
-		<!-- a vore -->
-		var pushNotification = window.plugins.pushNotification;
-		pushNotification.register(app.successHandler, app.errorHandler,{"senderID":"675581025503","ecb":"app.onNotificationGCM"});
-		
-		
-		
     },
 	successHandler: function(result) {
    		alert('Callback Success! Result = '+result)
 	},
 	errorHandler:function(error) {
     	alert(error);
-	},
-	onNotificationGCM: function(e) {
-        switch( e.event )
-        {
-            case 'registered':
-                if ( e.regid.length > 0 )
-                {
-                    console.log("Regid " + e.regid);
-                    alert('registration id = '+e.regid);
-                }
-            break;
- 
-            case 'message':
-              // this is the actual push notification. its format depends on the data model from the push server
-              alert('message = '+e.message+' msgcnt = '+e.msgcnt);
-            break;
- 
-            case 'error':
-              alert('GCM error = '+e.msg);
-            break;
- 
-            default:
-              alert('An unknown GCM event has occurred');
-            break;
-        }
-    },
-	onNotification: function(e) {
-		$("#app-status-ul").append('<li>EVENT -> RECEIVED:' + e.event + '</li>');
-	
-		switch( e.event )
-		{
-		case 'registered':
-			if ( e.regid.length > 0 )
-			{
-				$("#app-status-ul").append('<li>REGISTERED -> REGID:' + e.regid + "</li>");
-				// Your GCM push server needs to know the regID before it can push to this device
-				// here is where you might want to send it the regID for later use.
-				console.log("regID = " + e.regid);
-			}
-		break;
-	
-		case 'message':
-			// if this flag is set, this notification happened while we were in the foreground.
-			// you might want to play a sound to get the user's attention, throw up a dialog, etc.
-			if ( e.foreground )
-			{
-				$("#app-status-ul").append('<li>--INLINE NOTIFICATION--' + '</li>');
-	
-				// on Android soundname is outside the payload.
-				// On Amazon FireOS all custom attributes are contained within payload
-				var soundfile = e.soundname || e.payload.sound;
-				// if the notification contains a soundname, play it.
-				var my_media = new Media("/android_asset/www/"+ soundfile);
-				my_media.play();
-			}
-			else
-			{  // otherwise we were launched because the user touched a notification in the notification tray.
-				if ( e.coldstart )
-				{
-					$("#app-status-ul").append('<li>--COLDSTART NOTIFICATION--' + '</li>');
-				}
-				else
-				{
-					$("#app-status-ul").append('<li>--BACKGROUND NOTIFICATION--' + '</li>');
-				}
-			}
-	
-		   $("#app-status-ul").append('<li>MESSAGE -> MSG: ' + e.payload.message + '</li>');
-			   //Only works for GCM
-		   $("#app-status-ul").append('<li>MESSAGE -> MSGCNT: ' + e.payload.msgcnt + '</li>');
-		   //Only works on Amazon Fire OS
-		   $status.append('<li>MESSAGE -> TIME: ' + e.payload.timeStamp + '</li>');
-		break;
-	
-		case 'error':
-			$("#app-status-ul").append('<li>ERROR -> MSG:' + e.msg + '</li>');
-		break;
-	
-		default:
-			$("#app-status-ul").append('<li>EVENT -> Unknown, an event was received and we do not know what it is</li>');
-		break;
-	  }
 	}
 };
