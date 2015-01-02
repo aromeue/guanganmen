8bca6cb2b8d11684c9f06d76f9c279502055a9cb
aromeue <alvaro@hitech-informatica.es>
2015-01-02 11:49:28 +0100

    v2.02-1149

--- js/PushInit.js
+++ js/PushInit.js
@@ -17,201 +17,6 @@
  * under the License.
  */
 
-function registerPushwooshIOS() {
- 	var pushNotification = window.plugins.pushNotification;
-
- 	//push notifications handler
-	document.addEventListener('push-notification', function(event) {
-				var notification = event.notification;
-				navigator.notification.alert(notification.aps.alert);
-				
-				//to view full push payload
-				//navigator.notification.alert(JSON.stringify(notification));
-				
-				//reset badges on icon
-				pushNotification.setApplicationIconBadgeNumber(0);
-			  });
-
-	pushNotification.registerDevice({alert:true, badge:true, sound:true, pw_appid:"4F0C807E51EC77.93591449", appname:"Pushwoosh"},
-									function(status) {
-										var deviceToken = status['deviceToken'];
-										console.warn('registerDevice: ' + deviceToken);
-										onPushwooshiOSInitialized(deviceToken);
-									},
-									function(status) {
-										console.warn('failed to register : ' + JSON.stringify(status));
-										navigator.notification.alert(JSON.stringify(['failed to register ', status]));
-									});
-	
-	//reset badges on start
-	pushNotification.setApplicationIconBadgeNumber(0);
-}
-
-function onPushwooshiOSInitialized(pushToken)
-{
-	var pushNotification = window.plugins.pushNotification;
-	//retrieve the tags for the device
-	pushNotification.getTags(function(tags) {
-								console.warn('tags for the device: ' + JSON.stringify(tags));
-							 },
-							 function(error) {
-								console.warn('get tags error: ' + JSON.stringify(error));
-							 });
-	 
-	//start geo tracking. PWTrackSignificantLocationChanges - Uses GPS in foreground, Cell Triangulation in background. 
-	pushNotification.startLocationTracking('PWTrackSignificantLocationChanges',
-									function() {
-										   console.warn('Location Tracking Started');
-									});
-}
-
-function registerPushwooshAndroid() {
-
- 	var pushNotification = window.plugins.pushNotification;
-
-	//push notifications handler
-	document.addEventListener('push-notification', function(event) {
-	            var title = event.notification.title;
-	            var userData = event.notification.userdata;
-
-	            //dump custom data to the console if it exists
-	            if(typeof(userData) != "undefined") {
-					console.warn('user data: ' + JSON.stringify(userData));
-				}
-
-				//and show alert
-				navigator.notification.alert(title); <!-- Mostra alerta al registrar el dispositiu -->
-
-				//stopping geopushes
-				pushNotification.stopGeoPushes();
-			  });
-
-				//projectid: "GOOGLE_PROJECT_ID", appid : "PUSHWOOSH_APP_ID"
-				pushNotification.onDeviceReady({ projectid: "675581025503"/*, appid: "E9024-6EF1C"*/ });
-				
-				pushNotification.registerDevice(
-					function(token) {
-						//Alert amb el token de registre. Desactivat pq si no surt cada inici
-						alert("Registre: "+token); //Mostrar alert en lo token de registre
-						//callback when pushwoosh is ready
-						onPushwooshAndroidInitialized(token);
-					},
-					function(status) {
-						alert("failed to register: " +  status);
-						console.warn(JSON.stringify(['failed to register ', status]));
-					});
- }
- 
-
-
-function unregisterPushwoosh()
-{
-	var pushNotification = window.plugins.pushNotification;
-	pushNotification.unregisterDevice(function(token) {
-		alert("unregistered, old token " + token);
-	},
-	function(status) {
-		alert("failed to unregister: " +  status);
-		console.warn(JSON.stringify(['failed to unregister ', status]));
-	});
-}
-
-
-function onPushwooshAndroidInitialized(pushToken)
-{
-	//output the token to the console
-	console.warn('push token: ' + pushToken);
-
-	var pushNotification = window.plugins.pushNotification;
-	
-	pushNotification.getTags(function(tags) {
-							console.warn('tags for the device: ' + JSON.stringify(tags));
-							alert('tags for the device: ' + JSON.stringify(tags));
-						 },
-						 function(error) {
-							console.warn('get tags error: ' + JSON.stringify(error));
-						 });
-	 
-
-	//set multi notificaiton mode
-	//pushNotification.setMultiNotificationMode();
-	pushNotification.setEnableLED(true);
-	
-	//set single notification mode
-	//pushNotification.setSingleNotificationMode();
-	
-	//disable sound and vibration
-	//pushNotification.setSoundType(1);
-	//pushNotification.setVibrateType(1);
-	
-	pushNotification.setLightScreenOnNotification(false);
-	
-	//goal with count
-	//pushNotification.sendGoalAchieved({goal:'purchase', count:3});
-	
-	//goal with no count
-	//pushNotification.sendGoalAchieved({goal:'registration'});
-
-	//setting list tags
-	//pushNotification.setTags({"MyTag":["hello", "world"]});
-	
-	//settings tags
-	pushNotification.setTags({deviceName:"hello", deviceId:10},
-									function(status) {
-										console.warn('setTags success');
-									},
-									function(status) {
-										console.warn('setTags failed');
-									});
-		
-	function geolocationSuccess(position) {
-		pushNotification.sendLocation({lat:position.coords.latitude, lon:position.coords.longitude},
-								 function(status) {
-									  console.warn('sendLocation success');
-								 },
-								 function(status) {
-									  console.warn('sendLocation failed');
-								 });
-	};
-		
-	// onError Callback receives a PositionError object
-	//
-	function geolocationError(error) {
-		alert('code: '    + error.code    + '\n' +
-			  'message: ' + error.message + '\n');
-	}
-	
-	function getCurrentPosition() {
-		navigator.geolocation.getCurrentPosition(geolocationSuccess, geolocationError);
-	}
-	
-	//greedy method to get user position every 3 second. works well for demo.
-//	setInterval(getCurrentPosition, 3000);
-		
-	//this method just gives the position once
-//	navigator.geolocation.getCurrentPosition(geolocationSuccess, geolocationError);
-		
-	//this method should track the user position as per Phonegap docs.
-//	navigator.geolocation.watchPosition(geolocationSuccess, geolocationError, { maximumAge: 3000, enableHighAccuracy: true });
-
-	//Pushwoosh Android specific method that cares for the battery
-	pushNotification.startGeoPushes();
-}
-
- function initPushwoosh() {
-	var pushNotification = window.plugins.pushNotification;
-	if(device.platform == "Android")
-	{
-		registerPushwooshAndroid();
-		pushNotification.onDeviceReady();
-	}
-
-	if(device.platform == "iPhone" || device.platform == "iOS")
-	{
-		registerPushwooshIOS();
-		pushNotification.onDeviceReady();
-	}
-}
 
 function checkConnection() {
     var networkState = navigator.connection.type;
@@ -236,6 +41,29 @@ function checkConnection() {
 	}
 
 	//alert("url:"+window.location);
+}
+
+function registerPush() {
+	$("#app-status-ul").append("<li>Creant pushNotificaion</li>");
+	var pushNotification = window.plugins.pushNotification;
+	
+	$("#app-status-ul").append("<li>Registrant</li>");
+	var res = pushNotification.register(
+		function (token) {
+			$("#app-status-ul").append("<li>Token:"+token+"</li>");
+		},
+		function (error) {
+			$("#app-status-ul").append("<li>Error:"+error+"</li>");
+		},
+		{
+			"badge":"true",
+		    "sound":"true",
+		    "alert":"true",
+			"senderID":"675581025503",
+			"ecb":"onNotification"
+		});
+	$("#app-status-ul").append("<li>Resultat:"+res+"</li>");
+	
 }
 
 function initPush() {
